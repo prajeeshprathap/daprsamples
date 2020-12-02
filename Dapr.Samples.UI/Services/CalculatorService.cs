666e6763dcf9a2fa94d0a9f11d84ab38029c8efb
Prajeesh Prathap <prajeesh.prathap@gmail.com>
2020-12-02 11:06:42 +0100

    added skeleton with one service and a blazor app

---
+++ Dapr.Samples.UI/Services/CalculatorService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dapr.Samples.UI.Services
+{
+    public class CalculatorService
+    {
+        public async Task<int> Add(int a, int b, CancellationToken cancellationToken)
+        {
+            return a + b;
+        }
+        public async Task<int> Subtract(int a, int b, CancellationToken cancellationToken)
+        {
+            return a - b;
+        }
+        public async Task<int> Multiply(int a, int b, CancellationToken cancellationToken)
+        {
+            return a * b;
+        }
+        public async Task<int> Divide(int a, int b, CancellationToken cancellationToken)
+        {
+
+            return a / b;
+        }
+    }
+}
---
+++ Dapr.Samples.UI/Startup.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Samples.UI.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Dapr.Samples.UI
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages(); // Configuration for Blazor
+            services.AddServerSideBlazor(); // Configuration for Blazor
+            services.AddSingleton<CalculatorService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub(); // Configuration for Blazor
+                endpoints.MapFallbackToPage("/_Host"); // Configuration for Blazor
+            });
+        }
+    }
+}
